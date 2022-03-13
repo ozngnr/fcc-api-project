b63e1078a89df7799dc702b185ac5daffe4b2614
ozngnr <67223977+ozngnr@users.noreply.github.com>
2022-03-13 13:36:41 +0300

    added :date api endpoint

--- server.js
+++ server.js
@@ -4,29 +4,50 @@
 // init project
 var express = require('express');
 var app = express();
-
+var port = process.env.PORT || 3000;
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
-// so that your API is remotely testable by FCC 
+// so that your API is remotely testable by FCC
 var cors = require('cors');
-app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
+app.use(cors({ optionsSuccessStatus: 200 })); // some legacy browsers choke on 204
 
 // http://expressjs.com/en/starter/static-files.html
 app.use(express.static('public'));
 
 // http://expressjs.com/en/starter/basic-routing.html
-app.get("/", function (req, res) {
+app.get('/', function (req, res) {
   res.sendFile(__dirname + '/views/index.html');
 });
 
-
-// your first API endpoint... 
-app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
+// your first API endpoint...
+app.get('/api/hello', function (req, res) {
+  res.json({ greeting: 'hello API' });
 });
 
+app.get('/api', function (req, res) {
+  const now = new Date();
+  res.json({
+    unix: now.getTime(),
+    utc: now.toUTCString(),
+  });
+});
 
+app.get('/api/:date', (req, res) => {
+  const isDateString = req.params.date.includes('-');
+  const passedInValue = isDateString
+    ? req.params.date
+    : parseInt(req.params.date);
+  const date = new Date(passedInValue);
+  const unix = date.getTime();
+  const utc = date.toUTCString();
+
+  if (isNaN(unix)) {
+    res.json({ error: 'Invalid Date' });
+  } else {
+    res.json({ unix, utc });
+  }
+});
 
 // listen for requests :)
-var listener = app.listen(process.env.PORT, function () {
+var listener = app.listen(port, function () {
   console.log('Your app is listening on port ' + listener.address().port);
 });
